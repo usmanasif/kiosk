e9845e2962f09dc7c8d14f6be25d75d0815d2d7a
Usman Asif <usman.asif@devsinc.com>
2018-07-02 16:57:17 +0500

    Application: Implemented splash page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_controller?(controller_name)
+    params[:controller] == controller_name
+  end
+
+  def static_pages_controller?
+    active_controller?('static_pages')
+  end
 end
